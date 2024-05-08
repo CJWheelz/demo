2eaab904a05bc04f17d5a22b82862a6f073dea1e
CJ Wheelan <cjwheelan@gmail.com>
2024-05-08 00:01:28 -0400

    working final version

--- src/index.jsx
+++ src/index.jsx
@@ -1,44 +1,33 @@
 import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.scss';
-// import require from 'dotenv';
 
 import OpenAI from 'openai';
 
-// require('dotenv').config();
+const key = 'API_KEY_HERE';
 
-const openai = new OpenAI({ apiKey: process.env.API_KEY, dangerouslyAllowBrowser: true });
-
-// async function main() {
-//   const completion = await openai.chat.completions.create({
-//     messages: [{ role: 'system', content: 'You are a helpful assistant.' }],
-//     model: 'gpt-3.5-turbo',
-//   });
-
-//   console.log(completion.choices[0]);
-// }
+const openai = new OpenAI({ apiKey: key, dangerouslyAllowBrowser: true });
 
 function Welcome(props) {
   const [query, setQuery] = useState('');
   const [imgAddress, setImgAddress] = useState('');
   const [isLoading, setIsLoading] = useState(false);
+  const [selectedOption, setSelectedOption] = useState('2');
 
+  const handleDropdownChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
   const onQueryChange = (event) => {
     setQuery(event.target.value);
   };
 
-  // async function main() {
-  //   const image = await openai.images.generate({ prompt: query });
-
-  //   console.log(image.data[0].url);
-  //   setImgAddress(image.data[0].url);
-  // }
-
-  async function generate3() {
+  async function generate() {
     setIsLoading(true);
 
+    const modelNumber = `dall-e-${selectedOption}`;
+
     const response = await openai.images.generate({
-      model: 'dall-e-3',
+      model: modelNumber,
       prompt: query,
       n: 1,
       size: '1024x1024',
@@ -47,16 +36,6 @@ function Welcome(props) {
     setIsLoading(false);
     setImgAddress(response.data[0].url);
   }
-
-  // async function varyImage() {
-  //   const response = await openai.images.createVariation({
-  //     model: 'dall-e-2',
-  //     image: fs.createReadStream('images/photo.jpeg'),
-  //     n: 1,
-  //     size: '1024x1024',
-  //   });
-  //   image_url = response.data[0].url;
-  // }
 
   function renderLoad() {
     return (
@@ -74,11 +53,28 @@ function Welcome(props) {
   return (
     <div>
       <h1>Welcome to CallistoGPT!</h1>
+      <div className="container">
+        <div className="row align-items-end">
+          <div className="col-6 align-self-end">
+            <input type="text" name="Query" value={query} placeholder="Enter Your Image Query Here" id="" onChange={onQueryChange} />
+          </div>
 
-      <input type="text" name="Query" value={query} placeholder="Enter Your Image Query Here" id="" onChange={onQueryChange} />
+          <div className="col-3 align-self-start">
+            <h5>Which Model?</h5>
+            <select value={selectedOption} onChange={handleDropdownChange} className="form-select">
+              <option value="2">DALL-E-2</option>
+              <option value="3">DALL-E-3</option>
+            </select>
+          </div>
 
-      <button onClick={generate3} className="btn btn-primary" type="button">Generate</button>
+          <div className="col-3">
+            <button onClick={generate} className="btn btn-primary" type="button">Generate</button>
+          </div>
+        </div>
+      </div>
+
       <br />
+
       { isLoading ? renderLoad() : <div />}
 
       <img src={imgAddress} alt="" />
