3586e5aaf823ddb81553b5f94b78cf0271734a12
CJ Wheelan <cjwheelan@gmail.com>
2024-05-07 23:11:13 -0400

    broken API v2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import eslint from 'vite-plugin-eslint';
 import { defineConfig } from 'vite';
 import autoprefixer from 'autoprefixer';
 
+// const Dotenv = require('dotenv-webpack');
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
