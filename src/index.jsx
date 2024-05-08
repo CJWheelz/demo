6a26efd6430ef50c75d763cece9a2d732090ada1
CJ Wheelan <cjwheelan@gmail.com>
2024-05-07 22:18:46 -0400

    working openai api

--- src/index.jsx
+++ src/index.jsx
@@ -1,85 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
-import {
-  BrowserRouter, Routes, Route, NavLink, useParams,
-} from 'react-router-dom';
 import './style.scss';
 
 import OpenAI from 'openai';
-import Counter from './components/counter';
-import Controls from './components/controls';
 
-const openai = new OpenAI({ apiKey: 'sk-proj-qfQNej1PltxUxhbXKHMVT3BlbkFJzhUgfPX0ttYX0ABt76ps', dangerouslyAllowBrowser: true });
+const openai = new OpenAI({ apiKey: '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', dangerouslyAllowBrowser: true });
 
 // async function main() {
-//   const image = await openai.images.generate({ prompt: 'A cute baby sea otter' });
+//   const completion = await openai.chat.completions.create({
+//     messages: [{ role: 'system', content: 'You are a helpful assistant.' }],
+//     model: 'gpt-3.5-turbo',
+//   });
 
-//   console.log(image.data);
+//   console.log(completion.choices[0]);
 // }
 
-async function main() {
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: 'system', content: 'You are a helpful assistant.' }],
-    model: 'gpt-3.5-turbo',
-  });
-
-  console.log(completion.choices[0]);
-}
-
-function About(props) {
-  return <div> All there is to know about me </div>;
-}
 function Welcome(props) {
+  const [query, setQuery] = useState('');
+  const [imgAddress, setImgAddress] = useState('');
+
+  const onQueryChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  // async function main() {
+  //   const image = await openai.images.generate({ prompt: query });
+
+  //   console.log(image.data[0].url);
+  //   setImgAddress(image.data[0].url);
+  // }
+
+  async function generate3() {
+    const response = await openai.images.generate({
+      model: 'dall-e-3',
+      prompt: query,
+      n: 1,
+      size: '1024x1024',
+    });
+
+    setImgAddress(response.data[0].url);
+  }
+
   return (
     <div>
-      <div>Welcome</div>
+      <h1>Welcome to CallistoGPT!</h1>
 
-      <button onClick={main} type="button">Generate</button>
+      <input type="text" name="Query" value={query} placeholder="Enter Your Image Query Here" id="" onChange={onQueryChange} />
 
-      <Counter />
-      <Controls />
+      <button onClick={generate3} type="button">Generate</button>
+
+      <img src={imgAddress} alt="" />
     </div>
-  );
-}
-function Test(props) {
-  const { id } = useParams();
-  return <div> ID: {id} </div>;
-}
-function FallBack(props) {
-  return <div>URL Not Found</div>;
-}
-
-function Nav(props) {
-  return (
-    <nav>
-      <ul>
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/about">About</NavLink></li>
-        <li><NavLink to="/test/id1">test id1</NavLink></li>
-        <li><NavLink to="/test/id2">test id2</NavLink></li>
-      </ul>
-    </nav>
   );
 }
 
 function App(props) {
   return (
-    <BrowserRouter>
-      <div>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Welcome />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/test/:id" element={<Test />} />
-          <Route path="*" element={<FallBack />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div>
+      <Welcome />
+    </div>
   );
 }
-// function App() {
-//   return <div className="test">All the REACT are belong to us!</div>;
-// }
 
 const root = createRoot(document.getElementById('main'));
 root.render(<App />);
